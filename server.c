3d32e56e134f87ceb55716064759c1bf4936c17f
Huejii <94972402+Huejii@users.noreply.github.com>
2022-10-30 09:48:51 +0900

    sprintf

--- server.c
+++ server.c
@@ -64,7 +64,7 @@ int main()
 	        fp = fopen(filename, "w");
 	        fputs(writeString, fp);
             //추가 필요: write string 바이트 write하기
-            writeByte = _itoa(strlen(writeString));
+            sprintf(writeByte, "%s", strlen(writeString));
             printf("Write String Byte Size: %s\n", writeByte);
             write(fd2, writeByte,  strlen(writeByte)+1); //수정필요
 
