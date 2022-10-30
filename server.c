e15440cbd7d20a6653c42f9d1a8ee8cf7f3c3994
Huejii <94972402+Huejii@users.noreply.github.com>
2022-10-30 13:33:23 +0900

    아직..

--- server.c
+++ server.c
@@ -80,8 +80,14 @@ int main()
 	        fputs(writeString, fp);
             //아래 추가: write string 바이트 write하기
             sprintf(writeByte, "%ld", strlen(writeString));
+            printf("%c\n", writeByte[0]);
+            printf("%c\n", writeByte[1]);
+            printf("%c\n", writeByte[2]);
             printf("%ld\n", strlen(writeByte));
             writeByte[strlen(writeByte)] = '\0';
+            printf("%c\n", writeByte[0]);
+            printf("%c\n", writeByte[1]);
+            printf("%c\n", writeByte[2]);
             printf("%ld\n", strlen(writeByte));
             printf("Write String Byte Size: %s\n", writeByte);
             write(fd2, writeByte,  strlen(writeByte)+1); //수정필요
