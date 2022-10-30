c28986b210f15fc3c47b319adb61c457fb809403
Huejii <94972402+Huejii@users.noreply.github.com>
2022-10-30 18:37:35 +0900

    ㅈㅜㅅㅓㄱㄲㅏㅈㅣ

--- client.c
+++ client.c
@@ -8,18 +8,21 @@
 
 int main()
 {
+    /* fifo descriptor */
     int fd;
     int fd2;
 
-    // FIFO path
+    /* 서버와 IPC할 Read, write할 FIFO 경로 2개 초기화 */
     char * fifo1 = "/tmp/fifo1";
     char * fifo2 = "/tmp/fifo2";
 
+    /* IPC 과정 반복 실행 */
     while (1)
     {
+        /* FIFO open. client는 fifo1에서 write only, fifo2에서 read only */
         fd = open(fifo1, O_WRONLY);
         fd2 = open(fifo2, O_RDONLY);
-        if (fd < 0) {
+        if (fd == -1) {
             fprintf(stderr, "Pipe Failed");
             return 1;
         }
@@ -43,49 +46,55 @@ int main()
         char getFileString[512]="";
         char temp[512];
 
+        /* 파일명 입력 받기 */
         printf("Filename:");
         fgets(filename, 512, stdin);
-        filename[strlen(filename) - 1] = '\0';
-        write(fd, filename, strlen(filename)+1);
-        sleep(1);
+        filename[strlen(filename) - 1] = '\0'; // 개행문자 제거
+        write(fd, filename, strlen(filename)+1); // server에 보낼 파일명 write
+        
+        /* 파일명이 없을 시 while문 break */
         if(strcmp(filename, "")==0)
         {
             printf("filename 입력 오류\n");
             break;
         }
 
+        /*read write 여부 입력 받기*/
         printf("R/W:");
         fgets(rwType, 512, stdin);
-        rwType[strlen(rwType) - 1] = '\0';
-        write(fd, rwType, strlen(rwType)+1);
+        rwType[strlen(rwType) - 1] = '\0'; // 개행문자 제거
+        write(fd, rwType, strlen(rwType)+1); // server에 보낼 rw type write
 
         if (strcmp(rwType,"R")==0)
         {
+            /* 파일 엑세스 타입이 Read일 때 */
             printf("Byte Size:");
-            fgets(readByteSize, 512, stdin);
-            write(fd, readByteSize, strlen(readByteSize)+1);
-            read(fd2, getFileString, strlen(getFileString)+1);
-            printf("Success get String: %s", getFileString);
+            fgets(readByteSize, 512, stdin); // 사용자로부터 읽을 데이터 바이트 수 입력 받기
+            write(fd, readByteSize, strlen(readByteSize)+1); // 읽을 데이터 바이트 수 write
+            read(fd2, getFileString, strlen(getFileString)+1); // 서버가 write한 데이터 string read
+            printf("Success get String: %s", getFileString); // getFileString 출력
             read(fd2, temp, sizeof(temp));
-            printf("%s\n", temp);
+            printf("%s\n", temp); 
         } 
         else if (strcmp(rwType,"W")==0)
         {
+            /* 파일 엑세스 타입이 Write일 때 */
             printf("Write String:");
-            fgets(writeString, 512, stdin);
-            write(fd, writeString, strlen(writeString)+1);
-            // 아래 추가: read 쓴 데이터 string길이 받기
-            read(fd2, writeByte,  strlen(writeByte)+1);
-            printf("Success Write Byte Size: %s", writeByte);
-            //이래는 오류땜에 추가
-            read(fd2, temp, sizeof(temp));
-            printf("%s\n", temp); //나눠짐 자꾸...
+            fgets(writeString, 512, stdin); // 사용자로부터 작성할 데이터 string 입력 받기
+            write(fd, writeString, strlen(writeString)+1); // 서버로 보낼 피일에 쓸 데이터 string write
+            read(fd2, writeByte,  strlen(writeByte)+1); // 파일에 쓴 데이터 byte수 read
+            printf("Success Write Byte Size: %s", writeByte); // 파일에 쓴 데이터 byte수 출력
+
+            read(fd2, temp, sizeof(temp)); //
+            printf("%s\n", temp); //
         }else
         {
-            printf("오류");
+            /* R/W 입력 오류 처리 */
+            printf("RWtype 오류");
+            exit(1);
         }
-        // Print the read message
 
+        /*FIFO close*/
         close(fd);
         close(fd2);
     }
--- server.c
+++ server.c
@@ -11,22 +11,34 @@
 
 int main()
 {
+    /* fifo descriptor */
     int fd;
     int fd2;
 
-    // FIFO path
+    /* client와 IPC할 Read, write할 FIFO 경로 2개 초기화 */
     char * fifo1 = "/tmp/fifo1";
     char * fifo2 = "/tmp/fifo2";
     pid_t pid;
 
-    // Creating the named FIFO
-    mkfifo(fifo1, 0666);
-    mkfifo(fifo2, 0666);
+    /* FIFO 경로에 FIFO 생성 (성공시 0 반환, 실패시 -1 반환) */
+    if (mkfifo(fifo1, 0666) == -1) 
+    {
+        fprintf(stderr, "make fifo1 Failed");
+        return 1;
+    }
+    if (!mkfifo(fifo2, 0666) == -1) {
+        fprintf(stderr, "make fifo2 Failed");
+        return 1;
+    }
 
+    /* IPC 과정 반복 실행 */
     while (1)
     {
+        /* FIFO open. server는 fifo1에서 read only, fifo2에서 write only */
         fd = open(fifo1,O_RDONLY);
         fd2 = open(fifo2,O_WRONLY);
+
+        /* 성공시 0 반환, 실패시 -1 반환 */
         if (fd == -1) {
             fprintf(stderr, "Pipe Failed");
             return 1;
@@ -35,77 +47,94 @@ int main()
             fprintf(stderr, "Pipe Failed");
             return 1;
         }
+
+
+
+        /* 생성할 파일의 pointer 선언 */
         FILE *fp;
 
-        char filename[512] = "";
-        char rwType[512] = "";
-        char writeString[512] = "";
-        char readByteSize[512] = "";
-        char writeByte[512]="";
-        char getFileString[512]="";
+        /* read, write로 보낼 문자열 선언 */
+        char filename[512] = "";      // 파일명
+        char rwType[512] = "";        // Read/Write 타입
+        char writeString[512] = "";   // 사용자가 write할 데이터 string
+        char writeByte[512]="";       // 사용자가 Write힌 데이터 byte 수 
+        char readByteSize[512] = "";  // 사용자가 read 요청한 데이터 byte 수
+        char getFileString[512]="";   // 사용자가 read 요청한 데이터 string
         char temp[512] = "";
 
+
+
+        /* client가 사용자로부터 입력받은 파일명 read */
         read(fd, filename, 512);
+        /* 파일명이 없을 시 while문 break */
         if(strcmp(filename, "")==0)
         {
             break;
         }
-        printf("read file name: %s\n", filename);
+        printf("read file name: %s\n", filename); // read한 파일명 출력
 
-
+        /* client가 사용자로부터 입력받은 파일 엑세스 타입(R/W) read */
         read(fd, rwType, 512);
         printf("read rw Type: %s\n", rwType);
 
-        //child fork
+        /* child fork */
         pid = fork();
 
+        /* fork 실패 */
         if (pid < 0) {
             fprintf(stderr, "fork Failed");
             return 1;
         }
+        /*child 수행*/
         else if(pid == 0)
         {
-            printf("Child pid: %d\n", getpid());
+            printf("Child pid: %d\n", getpid()); // child processid 출력
+
             if(strcmp(rwType,"R")==0)
             {
-                read(fd, readByteSize, 512);
-                printf("read Byte Size: %s\n",readByteSize);
-	            fp = fopen(filename, "r");
-            //아래 추가: 바이트 수 만큼 파일읽고 읽은 것 write하기
-                fread(getFileString, atoi(readByteSize), 1, fp);
-                printf("Success get String: %s\n", getFileString);
-                write(fd2, getFileString,  strlen(getFileString)+1);
+                /* 파일 엑세스 타입이 Read일 때 */
+                read(fd, readByteSize, 512);   // 읽을 데이터 바이트 수 read
+                printf("read Byte Size: %s\n",readByteSize);   // read한 읽을 데이터 바이트 수 출력
+	            fp = fopen(filename, "r");     // read 용으로 파일 open
+                fread(getFileString, atoi(readByteSize), 1, fp);   // 파일로부터 readByteSize만큼 데이터 읽어 getFileString에 입력
+                printf("Success get String: %s\n", getFileString); // 읽기에 성공한 데이터 string 출력
+                write(fd2, getFileString,  strlen(getFileString)+1); // client에 보낼 데이터 string write
             } 
             else if(strcmp(rwType,"W")==0)
             {
-                read(fd, writeString, 512);
-                printf("Write String: %s\n", writeString);
-	            fp = fopen(filename, "w");
-	            fputs(writeString, fp);
-                //아래 추가: write string 바이트 write하기
-                sprintf(writeByte, "%ld", strlen(writeString));
-                printf("Write String Byte Size: %s\n", writeByte);
-                write(fd2, writeByte,  strlen(writeByte)+1); //수정필요
-                write(fd2, temp, strlen(temp)+1);
+                /* 파일 엑세스 타입이 Write일 때 */
+                read(fd, writeString, 512); // 파일에 쓸 데이터 string read
+                printf("Write String: %s\n", writeString); //read한 데이터 string 출력
+	            fp = fopen(filename, "w");  // write 용으로 파일 open
+	            fputs(writeString, fp);    // 파일에 write
+                
+                sprintf(writeByte, "%ld", strlen(writeString)); // 파일에 write한 데이터 byte 수 writeByte에 입력
+                printf("Write String Byte Size: %s\n", writeByte); // writeByte size 출력
+                write(fd2, writeByte,  strlen(writeByte)+1); // client로 보낼 writeByte size write
+                write(fd2, temp, strlen(temp)+1); 
             }
             else
             {
+                /* R/W 입력 오류 처리 */
                 printf("R/W Type 입력 오류\n");
                 exit(1);
             }
-
+            
+            /*파일 close*/
             fclose(fp);
 
+            /*FIFO close*/
             close(fd);
             close(fd2);
         }
+        // parent 수행(wait child)
         else
         {
-            int status;
-            printf("Parent pid: %d\n", getpid());
-            wait(&status);
-            printf("status %d\n", status);
-            printf("Child Complete\n");
+            int status; // child 종료 상태
+            printf("Parent pid: %d\n", getpid()); // parent processid 출력
+            wait(&status); // child 종료 wait
+            printf("status %d\n", status); // status 출력
+            printf("Child Complete\n");   // child 수행 및 응답 완료 메세지 출력
         }
     }
     return 0;
